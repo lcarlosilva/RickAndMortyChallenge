5df6c841298f5120cc76cb421ddc3f76ab11dac5
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-04-23 01:40:12 -0300

    feat: create and config domain module

---
+++ domain/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "br.com.luiz.domain"
+    compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+
+    testImplementation(libs.junit)
+}
---
+++ domain/src/main/kotlin/br/com/luiz/domain/di/DomainModule.kt
@@ -0,0 +1,8 @@
+package br.com.luiz.domain.di
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+fun domainModule(): Module {
+    return module {}
+}
