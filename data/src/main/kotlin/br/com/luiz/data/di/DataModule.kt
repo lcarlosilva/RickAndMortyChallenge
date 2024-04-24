eaf75aa67a1c1c39cbfe2baed2b08eadda7e9fcb
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-04-24 01:52:35 -0300

    feat: inject repository and service in koin

--- data/src/main/kotlin/br/com/luiz/data/di/DataModule.kt
+++ data/src/main/kotlin/br/com/luiz/data/di/DataModule.kt
@@ -1,8 +1,15 @@
 package br.com.luiz.data.di
 
+import br.com.luiz.data.api.RickAndMortyService
+import br.com.luiz.data.repository.RickAndMortyRepositoryImpl
+import br.com.luiz.domain.repository.RickAndMortyRepository
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 fun dataModule(): Module {
-    return module {}
+    return module {
+        factory<RickAndMortyRepository> { RickAndMortyRepositoryImpl(get()) }
+        factory<RickAndMortyService> { get<Retrofit>().create(RickAndMortyService::class.java) }
+    }
 }
