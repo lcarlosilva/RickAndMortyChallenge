ac6faac01055e09483cbd0623ab7eab45d933d64
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-04-24 01:57:09 -0300

    feat: add screen list character in main activity

--- app/src/main/kotlin/br/com/luiz/rickandmortychallenge/ui/view/MainActivity.kt
+++ app/src/main/kotlin/br/com/luiz/rickandmortychallenge/ui/view/MainActivity.kt
@@ -6,41 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.compose.collectAsLazyPagingItems
 import br.com.luiz.rickandmortychallenge.ui.theme.RickAndMortyChallengeTheme
+import br.com.luiz.rickandmortychallenge.ui.view.compose.characterListScreen
+import br.com.luiz.rickandmortychallenge.ui.viewmodel.CharactersListViewModel
+import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = koinViewModel<CharactersListViewModel>()
             RickAndMortyChallengeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    val listCharacters = viewModel.listCharacterState.collectAsLazyPagingItems()
+                    characterListScreen(characters = listCharacters)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RickAndMortyChallengeTheme {
-        Greeting("Android")
     }
 }
