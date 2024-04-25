204327647915f5901889bfe58c6da6887d42d32e
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-04-25 15:47:11 -0300

    refactor(organize): improvement in the organization of code functions

--- app/src/main/kotlin/br/com/luiz/rickandmortychallenge/ui/view/compose/DetailsCharacterBottomSheet.kt
+++ app/src/main/kotlin/br/com/luiz/rickandmortychallenge/ui/view/compose/DetailsCharacterBottomSheet.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -40,6 +41,7 @@ import br.com.luiz.domain.model.Origin
 import br.com.luiz.rickandmortychallenge.R
 import br.com.luiz.rickandmortychallenge.utils.colorStatus
 import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
@@ -65,131 +67,155 @@ fun detailsCharacterBottomSheet(
                         .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Image(
-                    painter = rememberAsyncImagePainter(item.image),
-                    contentDescription =
-                        stringResource(
-                            R.string.lbl_content_description_photo_character,
-                            item.name,
-                        ),
-                    alignment = Alignment.TopCenter,
-                    modifier =
-                        Modifier
-                            .height(200.dp)
-                            .fillMaxWidth()
-                            .clip(CutCornerShape(20.dp)),
-                )
+                headerImage(item)
                 Spacer(modifier = Modifier.height(24.dp))
-                Text(
-                    text = item.name,
-                    style = MaterialTheme.typography.headlineLarge,
-                    textAlign = TextAlign.Center,
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    val color = colorStatus(item)
-                    Box(
-                        modifier =
-                            Modifier
-                                .size(16.dp)
-                                .padding(4.dp)
-                                .clip(CircleShape)
-                                .background(color),
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = item.status, style = MaterialTheme.typography.labelSmall)
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(
-                            text = item.species,
-                            style =
-                                MaterialTheme.typography.bodyMedium.copy(
-                                    fontWeight = FontWeight.Bold,
-                                ),
-                        )
-                        Text(
-                            text = stringResource(R.string.lbl_species),
-                            style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
-                        )
-                    }
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(
-                            text = item.gender,
-                            style =
-                                MaterialTheme.typography.bodyMedium.copy(
-                                    fontWeight = FontWeight.Bold,
-                                ),
-                        )
-                        Text(
-                            text = stringResource(R.string.lbl_gender),
-                            style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
-                        )
-                    }
-                }
-                Column(
-                    modifier =
-                        Modifier
-                            .padding(start = 20.dp)
-                            .fillMaxWidth(),
-                    horizontalAlignment = Alignment.Start,
-                ) {
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = item.origin.name,
-                        style =
-                            MaterialTheme.typography.bodyMedium.copy(
-                                fontWeight = FontWeight.Bold,
-                            ),
-                    )
-                    Text(
-                        text = stringResource(R.string.lbl_origin),
-                        style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
-                    )
-                    Spacer(modifier = Modifier.height(6.dp))
-                    Text(
-                        text = item.location.name,
-                        style =
-                            MaterialTheme.typography.bodyMedium.copy(
-                                fontWeight = FontWeight.Bold,
-                            ),
-                    )
-                    Text(
-                        text = stringResource(R.string.lbl_location),
-                        style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
-                    )
-                }
+                contentCharacterInfos(item)
+                contentCharacterLocations(item)
                 HorizontalDivider(
                     modifier = Modifier.padding(vertical = 20.dp),
                     thickness = 1.dp,
                     color = Color.Gray,
                 )
-                Row(
-                    modifier =
-                        Modifier
-                            .padding(top = 10.dp, end = 10.dp)
-                            .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                ) {
-                    Button(onClick = {
-                        scope.launch { sheetState.hide() }.invokeOnCompletion {
-                            if (!sheetState.isVisible) {
-                                onDismissRequest()
-                            }
-                        }
-                    }) {
-                        Text(stringResource(R.string.lbl_btn_ok))
-                    }
+                footer(scope, sheetState, onDismissRequest)
+            }
+        }
+    }
+}
+
+@Composable
+private fun headerImage(item: Character) {
+    Image(
+        painter = rememberAsyncImagePainter(item.image),
+        contentDescription =
+            stringResource(
+                R.string.lbl_content_description_photo_character,
+                item.name,
+            ),
+        alignment = Alignment.TopCenter,
+        modifier =
+            Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .clip(CutCornerShape(20.dp)),
+    )
+}
+
+@Composable
+private fun contentCharacterInfos(item: Character) {
+    Text(
+        text = item.name,
+        style = MaterialTheme.typography.headlineLarge,
+        textAlign = TextAlign.Center,
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        val color = colorStatus(item)
+        Box(
+            modifier =
+                Modifier
+                    .size(16.dp)
+                    .padding(4.dp)
+                    .clip(CircleShape)
+                    .background(color),
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = item.status, style = MaterialTheme.typography.labelSmall)
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = item.species,
+                style =
+                    MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.Bold,
+                    ),
+            )
+            Text(
+                text = stringResource(R.string.lbl_species),
+                style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
+            )
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = item.gender,
+                style =
+                    MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.Bold,
+                    ),
+            )
+            Text(
+                text = stringResource(R.string.lbl_gender),
+                style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
+            )
+        }
+    }
+}
+
+@Composable
+private fun contentCharacterLocations(item: Character) {
+    Column(
+        modifier =
+            Modifier
+                .padding(start = 20.dp)
+                .fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = item.origin.name,
+            style =
+                MaterialTheme.typography.bodyMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                ),
+        )
+        Text(
+            text = stringResource(R.string.lbl_origin),
+            style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        Text(
+            text = item.location.name,
+            style =
+                MaterialTheme.typography.bodyMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                ),
+        )
+        Text(
+            text = stringResource(R.string.lbl_location),
+            style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
+        )
+    }
+}
+
+@Composable
+private fun footer(
+    scope: CoroutineScope,
+    sheetState: SheetState,
+    onDismissRequest: () -> Unit,
+) {
+    Row(
+        modifier =
+            Modifier
+                .padding(top = 10.dp, end = 10.dp)
+                .fillMaxWidth(),
+        horizontalArrangement = Arrangement.End,
+    ) {
+        Button(onClick = {
+            scope.launch { sheetState.hide() }.invokeOnCompletion {
+                if (!sheetState.isVisible) {
+                    onDismissRequest()
                 }
             }
+        }) {
+            Text(stringResource(R.string.lbl_btn_ok))
         }
     }
 }
