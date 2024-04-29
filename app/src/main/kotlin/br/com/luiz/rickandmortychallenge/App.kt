f076741f7f2bace6dea3aaecf22901dfa72c54be
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-04-29 14:30:40 -0300

    chore: optimize logs level

--- app/src/main/kotlin/br/com/luiz/rickandmortychallenge/App.kt
+++ app/src/main/kotlin/br/com/luiz/rickandmortychallenge/App.kt
@@ -6,12 +6,15 @@ import br.com.luiz.data.di.dataModule
 import br.com.luiz.domain.di.domainModule
 import br.com.luiz.rickandmortychallenge.di.appModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(this@App)
             modules(getModules())
         }
